3c750933f13e92864756a25d7c24f39238ea5f9d
houlijia <wohoulijia@163.com>
2025-09-23 21:15:46 +0800

    update spring boot project

--- springboot/src/main/java/com/java/springboot/controller/ParamsController.java
+++ springboot/src/main/java/com/java/springboot/controller/ParamsController.java
@@ -7,22 +7,25 @@ import org.springframework.web.bind.annotation.*;
  * @Date 2025-09-22 10:55
  * @Desc
  **/
+
+@RestController
 public class ParamsController {
+
     @RequestMapping(value = "/getTest1",method = RequestMethod.GET)
     public String getTest1(){
         return "GET请求1";
     }
 
     @RequestMapping(value = "/getTest2",method = RequestMethod.GET)
-//  http://localhost:8080/getTest2?nickname=xxx&phone=xxx
+//  http://localhost:8088/getTest2?nickname=xxx&phone=xxx
     public String getTest2(String nickname,String phone){
-        System.out.println("nickname"+nickname);
-        System.out.println("phone"+phone);
+        System.out.println("nickname:"+nickname);
+        System.out.println("phone:"+phone);
         return "GET请求2";
     }
 
     @RequestMapping(value = "/getTest3",method = RequestMethod.GET)
-//  http://localhost:8080/getTest2?nickname=xxx
+//  http://localhost:8088/getTest3?nickname=xxx
     public String getTest3(@RequestParam(value = "nickname",required = false)String name){
         /*@RequestParam可以进行参数映射，当请求参数名称与Controller的方法参数不一致时，可以如上所示进行注解，但会使得该参数是必须的，可以加上required = false使得该参数可选（传递与否都可以）*/
         System.out.println("nickname:"+name);
@@ -61,7 +64,7 @@ public class ParamsController {
     }
 
     @GetMapping("/test/**")
-//  http://localhost:8080/test/xxx/xxxx
+//  http://localhost:8088/test/xxx/xxxx
 //如果时/test/*的话：http://localhost:8080/test/a
     public String test(){
         return "通配符请求";
