09beed9778ad6e93e478babc6aee3c8e3b7b14d3
houlijia <wohoulijia@163.com>
2025-09-23 18:49:05 +0800

    update spring boot project

---
+++ springboot/src/main/java/com/java/springboot/controller/User.java
@@ -0,0 +1,29 @@
+package com.java.springboot.controller;
+
+/**
+ * @Author houlijia
+ * @Date 2025-09-22 09:08
+ * @Desc
+ **/
+public class User {
+
+    private String name;
+
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
